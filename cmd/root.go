ee7da7db62589172c3ed009170a11596b72e79dd
Jossue Gamez <jossue.gamez@armory.io>
2020-05-08 18:06:42 -0500

    Completed documentation, changes to logs and github actions

--- cmd/root.go
+++ cmd/root.go
@@ -84,6 +84,12 @@ func initLog() *logrus.Logger {
 	}
 	log.SetLevel(level)
 
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	logrus.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
+	log.SetFormatter(customFormatter)
+
 	return log
 }
 
